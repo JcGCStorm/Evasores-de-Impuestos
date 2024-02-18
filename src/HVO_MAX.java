07d205204cff1a17c6296cfe4b3408b0a2f0e2e4
Gallegos Cortes José Antonio <josegc918@ciencias.unam.mx>
2024-02-18 15:37:14 -0600

    Agregué el esqueleto de observador y de Sujeto, además de comentar un poco con JavaDOC, la clase HVO_MAX fue modificada ligeramente agregando su constructor

--- src/HVO_MAX.java
+++ src/HVO_MAX.java
@@ -1,3 +1,21 @@
-public class HVO_MAX {
+public class HVO_MAX implements Sujeto{
+
+    public HVO_MAX() {
+    }
+    
+    @Override
+    public void agregar(Observador observador) {
+        return;
+    }
+    
+    @Override
+    public void eliminar(Observador observador) {
+        return;
+    }
+
+    @Override
+    public void notificar(){
+    }
+
     //metodos
 }
