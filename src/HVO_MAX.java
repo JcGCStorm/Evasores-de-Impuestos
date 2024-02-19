3fbd21278160646d9df5dd5f79ec0e6423fc5555
Gallegos Cortes José Antonio <josegc918@ciencias.unam.mx>
2024-02-18 18:08:36 -0600

    Agregue los que faltaban, aún sin metodos

--- src/HVO_MAX.java
+++ src/HVO_MAX.java
@@ -1,21 +1,7 @@
-public class HVO_MAX implements Sujeto{
+public class HVO_MAX{
 
     public HVO_MAX() {
     }
     
-    @Override
-    public void agregar(Observador observador) {
-        return;
-    }
-    
-    @Override
-    public void eliminar(Observador observador) {
-        return;
-    }
-
-    @Override
-    public void notificar(){
-    }
-
     //metodos
 }
