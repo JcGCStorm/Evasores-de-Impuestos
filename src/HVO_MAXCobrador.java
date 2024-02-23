f683527b5ca1eea62b94d4efd614ad072147bca2
Rafael <rafaelcastro@ciencias.unam.mx>
2024-02-22 23:44:38 -0600

    Documentado

--- src/HVO_MAXCobrador.java
+++ src/HVO_MAXCobrador.java
@@ -1,12 +1,12 @@
 /**
- * Interfaz que define el comportamiento de HVO Max y que se implementa a sus paquetes de pago, uso del strategy pattern
- * 
- */public interface HVO_MAXCobrador {
+* Interfaz que define el comportamiento del cobrador de HVO Max y que se implementa a sus paquetes de pago. 
+*/
+public interface HVO_MAXCobrador {
     
     /**
-     * Cobra al usuario especificado.
+     * Cobra al usuario especificado
      * 
-     * @param usuario El usuario al que se le va a cobrar.
+     * @param usuario El usuario al que se le va a cobrar
      */
     public void cobrar(Usuario usuario);
 }
