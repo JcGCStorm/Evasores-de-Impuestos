bd2af56b3aa050a0b100c010dd2bc897263979be
Rafael <rafaelcastro@ciencias.unam.mx>
2024-02-22 22:50:11 -0600

    Se documento

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,17 +1,34 @@
+/**
+ * La clase CuentaBancaria representa una cuenta bancaria que contiene un saldo
+ * y pertenecerá a los usuarios
+ */
+
 public class CuentaBancaria {
     private double saldo;
     Usuario usuario;
 
+    /**
+     * Constructor para inicializar una CuentaBancaria con un saldo inicial
+     * @param saldoInicial El saldo inicial de la cuenta bancaria
+     */
     public CuentaBancaria(double saldoInicial) {
         this.saldo = saldoInicial;
     }
 
+    /**
+     * Obtiene el saldo actual de la cuenta bancaria
+     * @return El saldo actual de la cuenta bancaria
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Realiza un pago desde la cuenta bancaria, disminuyendo el saldo
+     * @param monto El monto del pago a realizar
+     */
     public void realizarPago(double monto) {
-            saldo -= monto;
+        saldo -= monto;
     }
 
 }
